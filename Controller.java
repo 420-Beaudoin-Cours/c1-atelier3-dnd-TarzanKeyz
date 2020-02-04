fe03e42e3a831cf87dc0b2f2a7f8caa00313fe90
TarzanKeyz <dannybedard1@gmail.com>
2020-02-04 17:09:18 -0500

    Fini Atelier 3

--- Controller.java
+++ Controller.java
@@ -1,29 +1,112 @@
 package sample;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 
 public class Controller {
     public TextField txfSource;
-    public Label labTarget;
+    public ImageView imgCorbeille;
+    public ImageView imgCorbeille1;
+    public TextField txfSource2;
+    public ComboBox cmbTarget2;
+
+
+    //Exercice 1
+
 
     public void txfSourceDragDetected(MouseEvent mouseEvent) {
         TextField ref = (TextField) mouseEvent.getSource();
-        Dragboard db = ref.startDragAndDrop(TransferMode.ANY);
-        ClipboardContent content = new ClipboardContent();
-        content.putString(ref.getText());
-        db.setContent(content);
+        if (!ref.getText().isEmpty()) {
+            Dragboard db = ref.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(txfSource.getText());
+            db.setContent(content);
+        }
     }
 
-    public void labTargetDragOver(DragEvent dragEvent) {
+    public void imgTargetDragOver(DragEvent dragEvent) {
         dragEvent.acceptTransferModes(TransferMode.ANY);
     }
 
-    public void labTargetDragDrop(DragEvent dragEvent) {
+    public void imgTargetDragDrop(DragEvent dragEvent) {
         TextField refSource = (TextField) dragEvent.getGestureSource();
-        Label refTarget = (Label) dragEvent.getGestureTarget();
         Dragboard db = dragEvent.getDragboard();
-        refTarget.setText(db.getString());
+        refSource.clear();
+    }
+
+
+    //Exercice 2
+
+
+    public void txfSource2DragDetected(MouseEvent mouseEvent) {
+        if (!txfSource2.getText().isEmpty()) {
+            Dragboard db = txfSource2.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent facher = new ClipboardContent();
+            facher.putString(txfSource2.getText());
+            db.setContent(facher);
+        }
+    }
+
+    public void cmbTarget2DragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void cmbTarget2DragDrop(DragEvent dragEvent) {
+        Dragboard db = dragEvent.getDragboard();
+        cmbTarget2.getItems().add(db.getString());
+        txfSource2.clear();
+    }
+
+
+    //Exercice 3
+
+
+    public void txfSource3DragDetected(MouseEvent mouseEvent) {
+        TextField ref = (TextField) mouseEvent.getSource();
+        if(!ref.getText().isEmpty()) {
+            Dragboard db = ref.startDragAndDrop(TransferMode.ANY);
+            ClipboardContent empathique = new ClipboardContent();
+            empathique.putString(ref.getText());
+            db.setContent(empathique);
+        }
+    }
+
+    public void imgTarget3DragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void imgTarget3DragDrop(DragEvent dragEvent) {
+        Dragboard db = dragEvent.getDragboard();
+        TextField refSource = (TextField) dragEvent.getGestureSource();
+        refSource.clear();
+    }
+
+
+    //Exercice 4
+
+
+    public void txf4DragDetected(MouseEvent mouseEvent) {
+        TextField refSource = (TextField) mouseEvent.getSource();
+        Dragboard db = refSource.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent triste = new ClipboardContent();
+        triste.putString(refSource.getText());
+        db.setContent(triste);
+    }
+
+    public void txf4DragOver(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void txf4DragDrop(DragEvent dragEvent) {
+        TextField refSource = (TextField) dragEvent.getGestureSource();
+        TextField refTarget = (TextField) dragEvent.getGestureTarget();
+        if (!refSource.getText().isEmpty()) {
+            Dragboard db = dragEvent.getDragboard();
+            refTarget.setText(db.getString());
+            refSource.clear();
+        }
     }
 }
